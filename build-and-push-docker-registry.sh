d98344a7ffed2a6812b71132328b1bcdf15a80f8
Alex <a.garcia.ramirez19@gmail.com>
2020-11-06 15:16:56 +0100

    - Added error control - Some comment changes and variable revision in CityController.java - Polished build-and-push-docker-registry.sh

--- build-and-push-docker-registry.sh
+++ build-and-push-docker-registry.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+
+DOCKER_ACC="revenge19"
+DOCKER_REPO="weather-api"
+IMG_TAG="latest"
+
 mvn clean install
-docker build -t "revenge19/weather-api:latest" .
-docker push "revenge19/weather-api:latest"
+docker build -t "$DOCKER_ACC/$DOCKER_REPO:$IMG_TAG" .
+docker push "$DOCKER_ACC/$DOCKER_REPO:$IMG_TAG"
 
--- src/main/java/com/LTS/WeatherApi/controller/CityController.java
+++ src/main/java/com/LTS/WeatherApi/controller/CityController.java
@@ -2,30 +2,44 @@ package com.LTS.WeatherApi.controller;
 
 import com.LTS.WeatherApi.model.City;
 import com.LTS.WeatherApi.model.WeatherResponse;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.server.ResponseStatusException;
 
-
+@SuppressWarnings("unused")
 @RestController
 public class CityController {
-    //Declaring String with API Key as value
-    private static final String apikey = "ad43400b220daeedd0e9bfcf751bdf0c";
-    //Creating endpoint
+    @Value("${weather.apikey}")
+    private String apikey;
+
     @GetMapping("/weather/{city}")
-    //Adding Path Variable anotation for URL
     public City getReqCity(@PathVariable("city") String reqCity) {
         String url = "http://api.openweathermap.org/data/2.5/weather?q=" + reqCity + "&appid=" + apikey;
         RestTemplate restTemplate = new RestTemplate();
         WeatherResponse weatherResponse = restTemplate.getForObject(url, WeatherResponse.class);
+        if (weatherResponse == null) {
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND, String.format("The city %shas not been found", reqCity)
+            );
+        }
+        float celsius;
         //JSON gives us temp in Kelvin, doing the maths to get it to celsius
-        float celsius = ((float) weatherResponse.getMain().getTemp() - 273.15F);
-        //could've been done with DecimalFormat
-        City city = new City(weatherResponse.getWeather().get(0).getDescription(), String.format("%.2f", celsius));
-        return city;
+        celsius = ((float) weatherResponse.getMain().getTemp() - 273.15F);
+        return new City(weatherResponse.getWeather().get(0).getDescription(), String.format("%.2f", celsius));
     }
+    @Controller
+    public static class IndexController implements ErrorController {
+        private final static String PATH = "/error";
+        @Override
+        @RequestMapping(PATH)
+        @ResponseBody
+        public String getErrorPath() {
+            return "No Mapping Found";
+        }
 
+    }
 }
